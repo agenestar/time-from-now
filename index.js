0247109b46ffb4bfced5c861ac1ecff7aef90edc
Andreu Genestar <andreugenestar@outlook.com>
2016-08-31 16:18:02 +0200

    First version TESTS NOT OK

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 'use strict';
 
-module.exports = function() {
-  
+module.exports = function(ts) {
+  var now = Math.round(Date.now() / 1000)
+  ts = Math.round(new Date(ts).getTime() / 1000)
+  var diff = now - ts
+
+  console.log(`now-${now} ts-${ts} diff-${diff}`)
+
+  switch (true) {
+  	case diff < 60:
+    	return 'Ahora'
+    case diff < 3600:
+    	var mins = Math.floor(diff / 60);
+    	return 'Hace ' + (mins === 1 ? 'un minuto' : mins + ' minutos');
+    case diff < 86400:
+    	var hours = Math.floor(diff / 3600);
+    	return 'Hace ' + (hours === 1 ? 'una hora' : hours + ' horas');
+    case diff < 2678400:
+    	var days = Math.floor(diff / 86400);
+    	return 'Hace ' + (days === 1 ? 'un dia' : days + ' dias');
+    case diff < 32140800:
+    	var months = Math.floor(diff / 2678400);
+    	return 'Hace ' + (months === 1 ? 'un mes' : months + ' meses');
+    default:
+    	var years = Math.floor(diff / 32140800);
+    	return 'Hace ' + (years === 1 ? 'un año ' : years + ' años');
+  }
 }
--- test.js
+++ test.js
@@ -9,24 +9,26 @@ const day = hour * 24
 const month = day * 30
 const year = month * 12
 
-tap.equal(new Date(), 'Ahora')
+const now = new Date()
 
-tap.equal(new Date() - minute, 'Hace un minuto')
-tap.equal(new Date() - minute * 2, 'Hace 2 minutos')
-tap.equal(new Date() - minute * 59, 'Hace 59 minutos')
+tap.equal(timeFromNow(now), 'Ahora')
 
-tap.equal(new Date() - hour, 'Hace una hora')
-tap.equal(new Date() - hour * 2, 'Hace 2 horas')
-tap.equal(new Date() - hour * 23, 'Hace 23 horas')
+tap.equal(timeFromNow(now - minute), 'Hace un minuto')
+tap.equal(timeFromNow(now - (minute * 2)), 'Hace 2 minutos')
+tap.equal(timeFromNow(now - (minute * 59)), 'Hace 59 minutos')
 
-tap.equal(new Date() - day, 'Hace un dia')
-tap.equal(new Date() - day * 2, 'Hace 2 dias')
-tap.equal(new Date() - day * 29, 'Hace 29 dias')
+tap.equal(timeFromNow(now - hour), 'Hace una hora')
+tap.equal(timeFromNow(now - (hour * 2)), 'Hace 2 horas')
+tap.equal(timeFromNow(now - (hour * 23)), 'Hace 23 horas')
 
-tap.equal(new Date() - month, 'Hace un mes')
-tap.equal(new Date() - month * 2, 'Hace 2 meses')
-tap.equal(new Date() - month * 11, 'Hace 11 meses')
+tap.equal(timeFromNow(now - day), 'Hace un dia')
+tap.equal(timeFromNow(now - (day * 2)), 'Hace 2 dias')
+tap.equal(timeFromNow(now - (day * 29)), 'Hace 29 dias')
 
-tap.equal(new Date() - year, 'Hace un año')
-tap.equal(new Date() - year * 2, 'Hace 2 años')
-tap.equal(new Date() - year * 23, 'Hace 23 años')
+tap.equal(timeFromNow(now - month), 'Hace un mes')
+tap.equal(timeFromNow(now - (month * 2)), 'Hace 2 meses')
+tap.equal(timeFromNow(now - (month * 11)), 'Hace 11 meses')
+
+tap.equal(timeFromNow(now - year), 'Hace un año')
+tap.equal(timeFromNow(now - (year * 2)), 'Hace 2 años')
+tap.equal(timeFromNow(now - (year * 23)), 'Hace 23 años')
